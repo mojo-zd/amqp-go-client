ee5d7e5fcbacd1bd3a9688ca242685170b9cc115
mojo <zsy@360loyo.com>
2015-11-16 18:16:06 +0800

    搭建基础架构

---
+++ src/const/contants.go
@@ -0,0 +1,13 @@
+package constants
+
+const (
+	BuzzTask     = "task"
+	BuzzReport   = "report"
+	BuzzWorkflow = "workflow"
+
+	CreateOperation   = "create"
+	FinishedOperation = "finished"
+	ReviewedOperation = "reviewed"
+	AcceptOperation   = "accept"
+	RejectOperation   = "reject"
+)
